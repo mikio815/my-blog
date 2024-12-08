20b3025039531b99cb32c9001b2aa177b03e066c
mikio815 <uaru0815@gmail.com>
2024-12-08 10:50:14 +0900

    change about

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Next.js Blog Starter Kit",
-    description: "Clone and deploy your own Next.js portfolio in minutes.",
+    title: "Portfolio by UaRu",
+    description: "Here is the portfolio site of UaRu, a freelance illustrator.",
     image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
   };
 
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@youalulu" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
